0556c841996f3997354400a56f8198301bcee1b0
JCPedroza <nemesis4go10@hotmail.com>
2024-01-12 20:08:23 -0600

    feat: add php

--- webdev.sh
+++ webdev.sh
@@ -1,9 +1,15 @@
+#!/bin/bash
+
+# php: server-side, html-embedded scripting language
+lang="php"
+
 # httpie: http and api testing
+# wuzz: unteractive console tool for HTTP inspection
 network="httpie wuzz"
 
-suite="$network"
-apt_install="sudo apt-get install $suite"
+suite="$lang $network"
 apt_update="sudo apt-get update"
+apt_install="sudo apt-get install $suite"
 $apt_update && $apt_install
 
 # nvm: node version manager (node is a js runtime)
