6efefbdf164d53b16eece02c7a36ecf63cb0d7b4
JCPedroza <nemesis4go10@hotmail.com>
2024-01-22 05:49:33 -0600

    feat: add neovim and kate

--- general-dev.sh
+++ general-dev.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+# code: gui text editor (vs code)
+# sublime-text: gui text editor
+# emacs: command line and gui text editor
+# vim: command line text editor
+# neovim: command line text editor
+# kate: gui text editor
+editor="code sublime-text emacs vim neovim kate"
+
 # git: distributed version control system
 # git-gui: gui for git commits
 # gitk: gui for git browsing
 # gh: github command line tools
 git="git git-gui gitk gh"
 
-# emacs: command line and gui text editor
-# vim: command line text editor
-# sublime-text: gui text editor
-# code: gui text editor (vs code)
-editor="emacs vim sublime-text code"
-
-suite="$git $editor"
+suite="$editor $git"
 apt_update="sudo apt-get update"
 apt_install="sudo apt-get install $suite"
 
